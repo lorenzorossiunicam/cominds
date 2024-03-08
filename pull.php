bbaf53bb13809b4dfcb3c0bfb701702cff972729
Lorenzo Rossi <lorenzo.rossi@unicam.it>
2024-03-08 21:49:34 +0100

    2024

--- pull.php
+++ pull.php
@@ -1,1 +1,1 @@
-<?php shell_exec( 'cd /var/www/html/cominds; git reset --hard origin/2023; git clean -d -f; git pull https://github.com/lorenzorossiunicam/cominds 2023' ); ?>
+<?php shell_exec( 'cd /var/www/html/cominds; git reset --hard origin/2024; git clean -d -f; git pull https://github.com/lorenzorossiunicam/cominds 2024' ); ?>
